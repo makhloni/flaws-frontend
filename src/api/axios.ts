ebdcdb7d932e95c6cdcc555828a58ccf96cbfc67
unknown <andile593>
2026-03-14 11:30:22 +0200

    Change of axios baseUrl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL // baseURL: "https://flaws-production.up.railway.app"
+  baseURL: "https://flaws-production.up.railway.app"
 });
 
 api.interceptors.request.use((config) => {
